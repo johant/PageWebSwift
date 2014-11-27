e1e8d3ac68411ba5a219f76aa88eff8628590190
Johan Torroledo <jtorroledo@icloud.com>
2014-11-27 11:09:50 -0500

    Page Web EscualIT

--- URLPage/ViewController.swift
+++ URLPage/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var pageWebView: UIWebView!
+
+    
+    @IBAction func backtoButtonTouchupInside(sender: UIButton) {
+        
+        var urlLink = NSURL(string: "http://historiasenlinea.com/demosalud")
+        var urlPagina = NSURLRequest(URL: urlLink!)
+        
+        pageWebView.loadRequest(urlPagina)
+        
+        
+    }
+    
+    @IBAction func gotoButtonTouchupInside(sender: UIButton) {
+        var urlLink = NSURL(string: "http://historiasenlinea.com/demosalud")
+        var urlPagina = NSURLRequest(URL: urlLink!)
+        
+        pageWebView.loadRequest(urlPagina)
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
